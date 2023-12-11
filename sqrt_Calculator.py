de3de04461211a0ecd817fee704ae24e3aef7cae
SUFIYANU IBRAHIM <113891807+Sisa4real@users.noreply.github.com>
2023-12-11 10:58:59 +0100

    Exercises Upload

---
+++ sqrt_Calculator.py
@@ -0,0 +1,5 @@
+number = eval(input("Enter the number: ")) #Collecting a number from user
+square_root = number**(1/2) #formular for calculating the square root
+print("The square root of the number {} is {}".format(number, square_root))
+
+print("Am done")
